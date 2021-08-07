d5a20589b7cf9b76a94df6f32eb68fcbaf0dadfa
yuiseki <yuiseki@gmail.com>
2021-08-07 17:44:54 +0900

    fix link structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,31 +2,25 @@
 import React from 'react';
 import 'twin.macro';
 import NextLink from 'next/link';
-import { Link, List, ListItem, Typography } from '@material-ui/core';
+import { Link, List, ListItem } from '@material-ui/core';
 import { Layout } from '~/components/Layout';
 import { states } from 'detect-location-jp';
 
 export const Page: React.VFC = () => {
   return (
     <Layout>
+      <h2>ハートフルマップへようこそ！</h2>
       <p>
-        <Typography variant='h6'>ハートフルマップへようこそ！</Typography>
+        <b>
+          <NextLink href='/auth/signup'>ユーザー登録</NextLink>
+        </b>
+        または
+        <b>
+          <NextLink href='/auth/signin'>ログイン</NextLink>
+        </b>
+        することで、口コミの投稿ができます。
       </p>
-      <p>
-        <Typography variant='h6'>
-          <b>
-            <NextLink href='/auth/signup'>ユーザー登録</NextLink>
-          </b>
-          または
-          <b>
-            <NextLink href='/auth/signin'>ログイン</NextLink>
-          </b>
-          することで、口コミの投稿ができます。
-        </Typography>
-      </p>
-      <p>
-        <Typography variant='h6'>都道府県を選択してください。</Typography>
-      </p>
+      <p>都道府県を選択してください。</p>
       <List>
         {states
           .sort((a, b) => {
@@ -37,9 +31,7 @@ export const Page: React.VFC = () => {
               <ListItem key={state.id}>
                 <Link>
                   <NextLink href={'/place/' + state.state_ja}>
-                    <span>
-                      {state.code} {state.state_ja}
-                    </span>
+                    {state.code + ' ' + state.state_ja}
                   </NextLink>
                 </Link>
               </ListItem>
